67820b918c350e16ea33316077354b41e19435cf
TheCodeScientist <jesse.stewart@codescientists.com>
2021-03-17 12:44:11 -0500

    Navigating to hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,24 +9,18 @@ import { MessageService } from '../message.service'
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[] = []
-  selectedHero?: Hero
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    this.getHeroes()
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes)
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
